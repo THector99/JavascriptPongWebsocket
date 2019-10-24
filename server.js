9f8aed8983acb78155ea651e0d8af4e000bdfd71
THector99 <timsport22@live.nl>
2019-10-24 14:34:13 +0200

    reuploaded everything after great cleanup

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', function(ws){
+  console.log('Client connected');
+  
+  ws.on('message', function(message){
+	  console.log("Received: "+ message);
+	  
+	  s.clients.forEach(function e(client){
+		client.send(message);
+			});
+  });
+  
+  ws.on('close', function(){
+  console.log('Client disconnected')
+  });
+ 
+});
